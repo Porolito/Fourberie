58d43efbb6c6c517a8edaa7c3121a36d877eea4c
Axel <axel.riou405@gmail.com>
2026-01-31 21:11:00 +0100

    ball gameplay WIP

--- Assets/Scripts/BallPath.cs
+++ Assets/Scripts/BallPath.cs
@@ -5,12 +5,37 @@ public class BallPath : MonoBehaviour
 {
     private Vector3 _startPos;
     
-    [SerializeField] private float _endPosX = 30; //X pos to reach
-    [SerializeField] private float _speed; //Speed of sine wave
-    [SerializeField] private float magnitude; //Magnitude of sine wave
+    public float endPosX; //X pos to reach
+    public float waveAmplitude;
+    public float waveFrequency;
+    public Vector3 spawnPosition = Vector3.zero;
+    
+    float moveDuration = 15;
+
+    public float startY = 1;
     // Update is called once per frame
-    void Update()
+    
+    
+    void Start()
     {
-        transform.DOMove(new Vector3(_endPosX,Mathf.Sin(Time.time * _speed) * magnitude, 0), 10);
+        _startPos = transform.position;
+        
+    }
+    private void TurnOffBall()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void GiveAPath()
+    {
+        transform.DOMoveX(endPosX, moveDuration).SetEase(Ease.Linear).OnComplete(TurnOffBall);
+
+        // Move along y-axis following the sine wave
+        DOVirtual.Float(0, moveDuration, moveDuration, (t) =>
+        {
+            // Calculate the new y position using the sine function and apply the shift to our og y
+            float newY = waveAmplitude * (Mathf.Sin(t * waveFrequency * 2) * startY);
+            transform.position = new Vector3(transform.position.x, _startPos.y + newY, _startPos.z);
+        }).SetEase(Ease.Linear);
     }
 }
