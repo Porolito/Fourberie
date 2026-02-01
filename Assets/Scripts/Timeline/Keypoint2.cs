243f24d19f2e5e829cd29da5ceca038ddf7075c0
Porolito <pierre.thomas16380@gmail.com>
2026-02-01 17:59:45 +0100

    save

--- Assets/Scripts/Timeline/Keypoint2.cs
+++ Assets/Scripts/Timeline/Keypoint2.cs
@@ -18,8 +18,8 @@ namespace Timeline
         [SerializeField] private DialogPartData[] dialogParts;
 
         private Coroutine currentPattern;
-        
-        private
+
+        private bool isHitChallengeActive = false;
 
         private void Awake()
         {
@@ -34,6 +34,7 @@ namespace Timeline
             DialogManager.instance.PlayDialog(dialogParts[1]);
             yield return new WaitForSeconds(dialogParts[1].clip.length - 0.5f);
             bulletManager.LaunchCoroutine(0.5f, 1, "Straight", 15f);
+            isHitChallengeActive = true;
             //KP logic : bullets
             yield return null;
 
