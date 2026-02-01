6906a3d845d637caee0267f4904937a65936d0ee
Erwan <46385057+LaLoutreCosmique@users.noreply.github.com>
2026-02-01 16:20:30 +0100

    fix: animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,11 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    private static readonly int IsRunningAnimString = Animator.StringToHash("isRunning");
+    private static readonly int IsJumpingAnimString = Animator.StringToHash("isJumping");
+    private static readonly int DashAnimString = Animator.StringToHash("dash");
+    private static readonly int AttackAnimString = Animator.StringToHash("attack");
+    
     GlobalInputs m_Inputs;
     GlobalInputs.PlayerActions m_PlayerActions;
     Rigidbody2D m_Rb2d;
@@ -30,6 +35,10 @@ public class Player : MonoBehaviour
     private bool m_isGrounded => Physics2D.Raycast(transform.position, -Vector2.up, transform.localScale.y + 0.01f, m_GroundLayer);
 
     [SerializeField] private float m_InvincibilityDuration = 1f;
+    
+    [Header("Refs")]
+    [SerializeField] private Animator m_Animator;
+    [SerializeField] private GameObject m_Visuals;
 
     [Header("Movement")]
     [SerializeField] private float m_MoveSpeed = 15f;
@@ -87,6 +96,7 @@ public class Player : MonoBehaviour
     private void OnTriggerStay2D(Collider2D other)
     {
         //TODO à finir avec les boules d'Axel + screenshake
+        if (!other.CompareTag("Projectile")) return;
         if (m_IsInvincible) return;
         print("HIT!");
         //EVENT HIT
@@ -126,10 +136,20 @@ public class Player : MonoBehaviour
     void HandleMovementInput(float value)
     {
         m_MoveInput = value;
-        if (value > 0)
-            m_AttackColliders.transform.localPosition = Vector2.right;
-        else if (value < 0)
-            m_AttackColliders.transform.localPosition = Vector2.left;
+        Vector3 scale = m_Visuals.transform.localScale;
+        switch (value)
+        {
+            case > 0:
+                m_AttackColliders.transform.localPosition = Vector2.right;
+                m_Visuals.transform.localScale = new Vector3(-Mathf.Abs(scale.x), scale.y, scale.z);
+                break;
+            case < 0:
+                m_AttackColliders.transform.localPosition = Vector2.left;
+                m_Visuals.transform.localScale = new Vector3(Mathf.Abs(scale.x), scale.y, scale.z);
+                break;
+            default:
+                break;
+        }
     }
 
     void HandleJumpInput()
@@ -137,7 +157,11 @@ public class Player : MonoBehaviour
         m_JumpInputReleased = false;
      
         if (m_isGrounded)
+        {
             m_IsJumping = true;
+            m_Animator.SetBool(IsJumpingAnimString, true);
+            m_Animator.SetBool(IsRunningAnimString, false);
+        }
         else
             StartJumpBuffer();
     }
@@ -173,6 +197,7 @@ public class Player : MonoBehaviour
         m_CanDash = false;
         m_IsDashing = true;
         m_DashVelocity = new Vector2(m_MoveInput, 0).normalized * m_DashSpeed;
+        m_Animator.SetTrigger(DashAnimString);
     }
 
     void HandleAttackInput()
@@ -185,6 +210,13 @@ public class Player : MonoBehaviour
     void MovePlayer()
     {
         Vector2 moveDir = new Vector2(m_MoveInput * m_MoveSpeed, -m_Gravity);
+        
+        if (m_isGrounded)
+        {
+            if (m_Animator.GetBool(IsJumpingAnimString))
+                m_Animator.SetBool(IsJumpingAnimString, false);
+            m_Animator.SetBool(IsRunningAnimString, m_MoveInput != 0f);
+        }
         
         if (!m_isGrounded)
             moveDir.y *= m_AirGravityMultiplier;
@@ -237,6 +269,7 @@ public class Player : MonoBehaviour
 
         m_CanAttack = false;
         m_AttackColliders.SetActive(true);
+        m_Animator.SetTrigger(AttackAnimString);
         StartCoroutine(AttackRoutine());
     }
 
@@ -251,5 +284,10 @@ public class Player : MonoBehaviour
     {
         yield return new WaitForSeconds(m_AttackCooldown);
         m_CanAttack = true;
+    }
+
+    public void PushAway(Vector2 dir)
+    {
+        m_Rb2d.linearVelocity = dir;
     }
 }
