cedd47af58fee5c5a87a322936b06d4b5bb7f2fc
Axel <axel.riou405@gmail.com>
2026-02-01 15:55:20 +0100

    new audio and audiomanager WIP

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,13 +1,39 @@
+using System;
 using UnityEngine;
 
 
-public enum SoundType
-{
-    Dash,
-    Win,
-    Loose
-}
 public class AudioManager : MonoBehaviour
 {
-    
+    private static AudioManager m_Instance;
+    private AudioSource audioSource;
+
+    private AudioSource playOnLoop;
+
+    private void Awake()
+    {
+        m_Instance = this;
+    }
+    private void Start()
+    {
+        audioSource =  GetComponent<AudioSource>();
+    }
+
+    public static void PlayOneShot(AudioClip soundToPlay, float volume = 1)
+    {
+        m_Instance.audioSource.PlayOneShot(soundToPlay, volume);
+    }
+
+    public static void PlayLooping(AudioClip soundToLoop, float volume = 1)
+    {
+        m_Instance.playOnLoop.clip = soundToLoop;
+        m_Instance.playOnLoop.volume = volume;
+        m_Instance.playOnLoop.loop = true;
+        m_Instance.playOnLoop.Play();
+    }
+
+    public static void StopLooping(AudioClip soundToStop)
+    {
+        m_Instance.playOnLoop.clip = soundToStop;
+        m_Instance.playOnLoop.Stop();
+    }
 }
